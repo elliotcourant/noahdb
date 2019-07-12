db596a8c8335658b5d1a0dee0e0cb8f57d27617b
Elliot Courant <me@elliotcourant.dev>
2019-07-12 16:50:46 -0500

    Tweaking coverage script again.

--- coverage.sh
+++ coverage.sh
@@ -3,14 +3,4 @@
 set -e
 echo "" > coverage.txt
 
-go test -v -race -coverprofile=profile.out -covermode=atomic -coverpkg=./...
-cat profile.out >> coverage.txt
-rm profile.out
-#
-#for d in $(go list ./... | grep -v vendor | grep -v pkg/ast); do
-#    go test -v -race -coverprofile=profile.out -covermode=atomic "$d"
-#    if [ -f profile.out ]; then
-#        cat profile.out >> coverage.txt
-#        rm profile.out
-#    fi
-#done
+go test -v -race -coverprofile=coverage.txt -covermode=atomic ./...
