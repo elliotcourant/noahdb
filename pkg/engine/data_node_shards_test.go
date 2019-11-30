c1e44ca836227d524b51aa7a0b98a2c664ead6dd
Elliot Courant <me@elliotcourant.dev>
2019-11-30 16:32:45 -0600

    Added test to ensure distributed unique constraint.

--- pkg/engine/data_node_shards_test.go
+++ pkg/engine/data_node_shards_test.go
@@ -39,4 +39,31 @@ func TestDataNodeContextBase_NewDataNodeShard(t *testing.T) {
 			NewDataNodeShard(1, 1, engine.DataNodeShardPosition_Leader)
 		assert.Error(t, err)
 	})
+
+	t.Run("distributed unique constraint", func(t *testing.T) {
+		cluster, cleanup := NewTestCoreCluster(t, 3)
+		defer cleanup()
+
+		txn1, err := cluster[0].Begin()
+		assert.NoError(t, err)
+
+		txn2, err := cluster[1].Begin()
+		assert.NoError(t, err)
+
+		_, err = txn1.
+			DataNodeShards().
+			NewDataNodeShard(1, 1, engine.DataNodeShardPosition_Leader)
+		assert.NoError(t, err)
+
+		_, err = txn2.
+			DataNodeShards().
+			NewDataNodeShard(1, 1, engine.DataNodeShardPosition_Leader)
+		assert.NoError(t, err)
+
+		err = txn1.Commit()
+		assert.NoError(t, err)
+
+		err = txn2.Commit()
+		assert.Error(t, err)
+	})
 }
